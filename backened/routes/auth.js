53ad966064459b69d83dca6a318ef6ccb6185f20
longwaytogofromnow <92814764+longwaytogofromnow@users.noreply.github.com>
2021-11-18 23:04:52 +0530

    50

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -45,8 +45,46 @@ router.post('/createuser',[
     }
      catch (error){
        console.error(error.message); 
-       res.status(500).send("some error occured");
+       res.status(500).send("Internal server error");
     }
+
+})
+
+// create a user using:POST "/api/auth/login". No login required
+router.post('/login',[
+  body('email','Enter a valid email').isEmail(),
+  body('password','Password cannot be blank').exists(),   
+] , async (req,res)=>{
+
+// If there are errors,returb Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+const {email,password} = req.body;
+try{
+
+  let user = await User.findOne({email});
+  if(!user){
+    return res.status(400).json({error:"Please try to login with a valid credentials"})
+  }
+  const passwordCompare = await bcrypt.compare(password,user.password)
+  if(!passwordCompare){
+    return res.status(400).json({error:"Please try to login with a valid credentials"}) 
+  }
+  const data={
+    user:{
+      id:user.id
+    }
+  }
+  const authtoken=jwt.sign(data,JWT_SECRET)
+  res.json(authtoken)
+
+}catch(error){
+    console.error(error.message); 
+    res.status(500).send("Internal server error");
+ }
 
 })
 
