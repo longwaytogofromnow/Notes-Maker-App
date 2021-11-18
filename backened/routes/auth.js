4393e5e623606779f9ac23baa88ddc753e82461a
longwaytogofromnow <92814764+longwaytogofromnow@users.noreply.github.com>
2021-11-18 18:08:33 +0530

    49

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -2,6 +2,10 @@ const express= require('express');
 const User = require('../models/User');
 const router= express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET= "Krishnaisagoodb$oy"
 
 // create a user using:POST "/api/auth/createuser". No login required
 router.post('/createuser',[
@@ -22,13 +26,22 @@ router.post('/createuser',[
     if(user){
       return res.status(400).json({error:"Sorry a user with this email already exists"})
     }
+    const salt= await bcrypt.genSalt(10);
+    const secPass= await bcrypt.hash(req.body.password,salt)
+    // create a new user
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass
       })
       
-      res.json(user)
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken=jwt.sign(data,JWT_SECRET)
+      res.json(authtoken)
     }
      catch (error){
        console.error(error.message); 
