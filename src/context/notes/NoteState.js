e078ad5d9a3034d0f9848fe8e240c881c4f2aa8e
longwaytogofromnow <92814764+longwaytogofromnow@users.noreply.github.com>
2021-11-21 09:42:57 +0530

    60

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,28 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
+import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name":"Krishna Kumar",
-        "class":"10 A1"
-    }
-    const [state, setState] = useState(s1)
-    const update=()=>{
-        setTimeout(() => {
-            setState({
-                "name":"Aman Kumar",
-                "class":"10 b"
-            })
-            
-        }, 1500);
-
-    }
-
+    const noteInitial=[{
+        "_id": "6196be419589b6c5ae53ee0f",
+        "user": "61968cd5225a932d9e570aab",
+        "title": "mytitle",
+        "description": "Wake up early",
+        "tag": "personal",
+        "date": "2021-11-18T20:57:37.563Z",
+        "__v": 0
+      },
+      {
+        "_id": "61975ca45c37041c77984df8",
+        "user": "61968cd5225a932d9e570aab",
+        "title": "Title 2",
+        "description": "do untill you die",
+        "tag": "motivation",
+        "date": "2021-11-19T08:13:24.107Z",
+        "__v": 0
+      }]
+      const [notes,setNotes]= useState(noteInitial)
 return(
-    <NoteContext.Provider value={{state,update}}>
+    <NoteContext.Provider value={{notes,setNotes}}>
         {props.children}
     </NoteContext.Provider>
 )
